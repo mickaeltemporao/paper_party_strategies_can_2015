513fb4d9dc95fbe0195243f80d59ef06cd0370c8
mickaeltemporao <mickael.temporao.1@ulaval.ca>
2016-12-10 16:49:05 -0500

    added non-informative feature list to rm

---
+++ src/dictionaries/removed_words.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+# ------------------------------------------------------------------------------
+# Title:        TODO: (add title)
+# Filename:     removed_words.R
+# Description:  TODO: (write me)
+# Version:      0.0.0.000
+# Created:      2016-12-10 16:36:57
+# Modified:     2016-12-10 16:48:31
+# Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
+# ------------------------------------------------------------------------------
+# Copyright (C) 2016 Mickael Temporão
+# Licensed under the GPL-2 < https://www.gnu.org/licenses/gpl-2.0.txt >
+# ------------------------------------------------------------------------------
+rm_words <- c("elxn42", "cdnpoli", "retweeted", "elxn", "aug", "sep", "oct",
+              "client", "www", "twitter", "blackberri", "iphone", "hootsuite",
+              "web", "tweetdeck", "iphon", "com", "green", "ndp", "lpc",
+              "canadiangreens", "liberal", "realchang", "android", "elizabethmay",
+              "trudeau", "mulcair", "ready4change", "twimg", "tom", "ipad",
+              "temptrudeau", "chang", "justin", "harper", "stephen", "justin",
+              "canadian", "will", "harper", "candid", "yyj", "today", "canada",
+              "gpc", "votegreen", "ago", "hour", "just", "want", "one", "make",
+              "get", "neet", "can", "say", "tpp", "last", "may", "qpgpc", "see",
+              "time", "teamtrudeau", "tmpm", "take", "tonight", "now", "watch",
+              "live", "day", "vancouv", "faceafacetva", "need", "let", "look",
+              "conservative", "munkdebate", "parti", "elizabeth",
+              "greenparty", "greenparti", "year", "macdeb", "t", "co", "s",
+              "party", "hq", "blackberry", "ca", "18", "realchange", "19",
+              "3", 0:31, letters, "re", "us", "globedebate", "hours")
